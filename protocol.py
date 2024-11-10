5e5d71d389cb6d98e8bae5765cb71306cbb0f7b2
MiroJelev <61290340+MiroJelev@users.noreply.github.com>
2024-11-10 09:57:36 +0200

    Add protocl

---
+++ protocol.py
@@ -0,0 +1,61 @@
+class State:
+
+  def __init__(self):
+    self.left_joystick_x = 32768  # [0, 65535], 2 bytes
+    self.left_joystick_y = 32768
+    self.left_trigger = 0  # [0, 255], 1 byte
+    self.right_joystick_x = 32768
+    self.right_joystick_y = 32768
+    self.right_trigger = 0
+    self.button_A = 0  # 0 or 1
+    self.button_B = 0
+    self.button_X = 0
+    self.button_Y = 0
+    self.button_L_SHOULDER = 0
+    self.button_R_SHOULDER = 0
+    self.button_L_THUMB = 0
+    self.button_R_THUMB = 0
+    self.button_BACK = 0
+    self.button_START = 0
+    self.button_DPAD = 0  # pyjoystick.HatValues, [0, 12]
+
+  def encode(self) -> bytes:
+    return bytes([
+        self.left_joystick_x >> 8,
+        self.left_joystick_x & 0xff,
+        self.left_joystick_y >> 8,
+        self.left_joystick_y & 0xff,
+        self.left_trigger,
+        self.right_joystick_x >> 8,
+        self.right_joystick_x & 0xff,
+        self.right_joystick_y >> 8,
+        self.right_joystick_y & 0xff,
+        self.right_trigger,
+        self.button_A << 0 | self.button_B << 1 | self.button_X << 2
+        | self.button_Y << 3 | self.button_L_SHOULDER << 4
+        | self.button_R_SHOULDER << 5 | self.button_L_THUMB << 6
+        | self.button_R_THUMB << 7,
+        self.button_BACK << 0 | self.button_START << 1,
+        self.button_DPAD,
+    ])
+
+  def decode(self, data: bytes) -> 'State':
+    ret = State()
+    ret.left_joystick_x = (data[0] << 8) + data[1]
+    ret.left_joystick_y = (data[2] << 8) + data[3]
+    ret.left_trigger = data[4]
+    ret.right_joystick_x = (data[5] << 8) + data[6]
+    ret.right_joystick_y = (data[7] << 8) + data[8]
+    ret.right_trigger = data[9]
+    ret.button_A = data[10] & 0x01
+    ret.button_B = data[10] & 0x02
+    ret.button_X = data[10] & 0x04
+    ret.button_Y = data[10] & 0x08
+    ret.button_L_SHOULDER = data[10] & 0x10
+    ret.button_R_SHOULDER = data[10] & 0x20
+    ret.button_L_THUMB = data[10] & 0x40
+    ret.button_R_THUMB = data[10] & 0x80
+    ret.button_BACK = data[11] & 0x01
+    ret.button_START = data[11] & 0x02
+    ret.button_DPAD = data[12]
+    return ret
--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-import pickle
 from pyjoystick.sdl2 import Key, Joystick, run_event_loop
 import socket
+import protocol
 
 HOST = socket.gethostname()#"127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-state = {}
+state = protocol.State()
 
 def print_add(joy):
   print('Added', joy)
@@ -17,47 +17,47 @@ def key_received(key: Key):
   if key.keytype == Key.KeyTypes.BUTTON:
     # print(key)
     if key.number == 0:
-      state['button_A'] = key.value
+      state.button_A = key.value
     elif key.number == 1:
-      state['button_B'] = key.value
+      state.button_B = key.value
     elif key.number == 2:
-      state['button_X'] = key.value
+      state.button_X = key.value
     elif key.number == 3:
-      state['button_Y'] = key.value
+      state.button_Y = key.value
     elif key.number == 4:
-      state['button_L_SHOULDER'] = key.value
+      state.button_L_SHOULDER = key.value
     elif key.number == 5:
-      state['button_R_SHOULDER'] = key.value
+      state.button_R_SHOULDER = key.value
     elif key.number == 6:
-      state['button_BACK'] = key.value
+      state.button_BACK = key.value
     elif key.number == 7:
-      state['button_START'] = key.value
+      state.button_START = key.value
     # elif key.number == 8: # no this button on steam deck
     #   pass
     elif key.number == 9:
-      state['button_L_THUMB'] = key.value
+      state.button_L_THUMB = key.value
     elif key.number == 10:
-      state['button_R_THUMB'] = key.value
+      state.button_R_THUMB = key.value
   elif key.keytype == Key.KeyTypes.AXIS:
     if key.number == 0:
-      state['left_joystick_x'] = int(
+      state.left_joystick_x = int(
           (key.value + 1) / 2 * 65535)  # [-1, 1] => [0, 65535]
     elif key.number == 1:
-      state['left_joystick_y'] = int(
+      state.left_joystick_y = int(
           (-key.value + 1) / 2 * 65535)  # reverse axis-Y value
     elif key.number == 2:
-      state['left_trigger'] = int(key.value * 255)  # [0, 1] => [0, 255]
+      state.left_trigger = int(key.value * 255)  # [0, 1] => [0, 255]
     elif key.number == 3:
-      state['right_joystick_x'] = int(
+      state.right_joystick_x = int(
           (key.value + 1) / 2 * 65535)  # [-1, 1] => [0, 65535]
     elif key.number == 4:
-      state['right_joystick_y'] = int(
+      state.right_joystick_y = int(
           (-key.value + 1) / 2 * 65535)  # reverse axis-Y value
     elif key.number == 5:
-      state['right_trigger'] = int(key.value * 255)  # [0, 1] => [0, 255]
+      state.right_trigger = int(key.value * 255)  # [0, 1] => [0, 255]
   elif key.keytype == Key.KeyTypes.HAT:
-    state['button_DPAD'] = key.value
-  print(state)
+    state.button_DPAD = key.value
+
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -69,11 +69,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}")
         while True:
-            # data = conn.recv(1024)
-            # if not data:
-            #     break
-            # conn.sendall(data)
-            conn.sendall(pickle.dumps(state))
+            conn.sendall(state.encode())
 
 
 run_event_loop(print_add, print_remove, key_received)
